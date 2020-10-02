9224e4fd7f4cfaa32efa5fe17d51b6bb07447d0e
rauanisanfelice <rauan.sanfelice@gmail.com>
2020-10-02 12:57:46 -0300

    Serrializers Produto e Categoria

---
+++ api/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+from api.models import Produto, Categoria
+
+
+class ProdutoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = Produto
+        fields = '__all__'
+
+
+
+class CategoriaSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = Categoria
+        fields = '__all__'
