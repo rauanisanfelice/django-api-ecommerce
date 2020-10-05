04aa6be72e54ff856380906f4fbe7f3658f91dcc
rauanisanfelice <rauan.sanfelice@gmail.com>
2020-10-05 08:28:19 -0300

    Corrigindo relacao das tabelas

--- api/models.py
+++ api/models.py
@@ -1,21 +1,6 @@
 from django.db import models
 
 
-class Categoria(models.Model):
-    
-    nome            = models.CharField(max_length=50)
-    descricao       = models.CharField(max_length=100, null=True, verbose_name='Descrição')
-    ativo           = models.BooleanField(default=True)
-    data_inclusao   = models.DateTimeField(auto_now=False, auto_now_add=True, verbose_name="Data de inclusão")
-
-    def __str__(self):
-        return self.nome
-
-    class Meta:
-        verbose_name = 'Categoria'
-        verbose_name_plural = 'Categorias'
-
-
 class Produto(models.Model):
 
     nome                = models.CharField(max_length=50)
@@ -29,11 +14,27 @@ class Produto(models.Model):
     data_exclusao   = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, verbose_name="Data de exclusão")
 
     user_inclusao   = models.CharField(max_length=50, verbose_name="Usuário inclusão")
-    user_alteracao  = models.CharField(max_length=50, verbose_name="Usuário alteração")
-    user_exclusao   = models.CharField(max_length=50, verbose_name="Usuário exclusão")
+    user_alteracao  = models.CharField(max_length=50, verbose_name="Usuário alteração", null=True)
+    user_exclusao   = models.CharField(max_length=50, verbose_name="Usuário exclusão", null=True)
 
     class Meta:
         verbose_name_plural = 'Produtos'
 
     def __str__(self):
         return self.nome
+
+
+class Categoria(models.Model):
+    
+    nome            = models.CharField(max_length=50)
+    descricao       = models.CharField(max_length=100, null=True, verbose_name='Descrição')
+    produto         = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    ativo           = models.BooleanField(default=True)
+    data_inclusao   = models.DateTimeField(auto_now=False, auto_now_add=True, verbose_name="Data de inclusão")
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
